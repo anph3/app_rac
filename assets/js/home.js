83589e1b2dbd880f80e026482449d011e9e1b242
Pham Hoang An (FTEL HiFPT) <anph13@fpt.com>
2024-01-04 14:14:25 +0700

    update screen OTP

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,7 +3,7 @@ var signInPage = document.getElementById('login');
 var signUpPage = document.getElementById('sign-up');
 
 function displayPage(pageName) {
-    var pages = ['home', 'login', 'sign-up'];
+    var pages = ['home', 'login', 'sign-up', 'forgot-password', 'otp'];
     for (var i = 0; i < pages.length; i++) {
         var page = document.getElementById(pages[i]);
         if (pages[i] === pageName) {
@@ -23,3 +23,41 @@ function displayPage(pageName) {
         currentPage.classList.remove('page-transition'); // Loại bỏ lớp page-transition sau 500ms
     }, 500);
 }
+
+var digitGroups = document.querySelectorAll('.digit-group');
+
+digitGroups.forEach(function(group) {
+    var inputs = group.querySelectorAll('input');
+
+    inputs.forEach(function(input, index) {
+        input.setAttribute('maxlength', '1');
+        input.addEventListener('keyup', function(e) {
+            var parent = this.parentElement;
+            var nextIndex = index + 1;
+            var prevIndex = index - 1;
+            if ((e.keyCode === 8 || e.keyCode === 37) && input.value === '') {
+                if (prevIndex >= 0) {
+                    inputs[prevIndex].removeAttribute('disabled');
+                    inputs[prevIndex].focus();
+                }
+            } else{
+                if (nextIndex < inputs.length && !(e.keyCode === 8 || e.keyCode === 37)) {
+                    inputs[nextIndex].removeAttribute('disabled');
+                    inputs[nextIndex].focus();
+                    inputs[nextIndex].value = null; // Xóa giá trị của input tiếp theo
+                } else {
+                    var autoSubmit = parent.getAttribute('data-autosubmit');
+                    if (autoSubmit) {
+                        parent.submit();
+                    }
+                }
+            }
+            if (!(input.value === '' && nextIndex=='1'))
+                input.setAttribute('disabled', 'disabled');
+        });
+    });
+});
+
+
+
+
